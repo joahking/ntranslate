ef143e01747ff268e9dc1e0e36ca5ab4a2419fed
Jorge Dias <jorge@mrdias.com>
2012-10-13 19:58:01 +0200

    Added named scope for pending translations in a language

--- app/models/text_resource.rb
+++ app/models/text_resource.rb
@@ -11,6 +11,11 @@ class TextResource < ActiveRecord::Base
 
   delegate :master_language, :to => :project
 
+  scope :pending_translation, lambda { |language|
+    joins("LEFT OUTER JOIN text_resource_translations ON text_resources.id = text_resource_translations.text_resource_id AND text_resource_translations.locale = '#{language}'").
+    where("translated = ? OR text_resource_translations.text_resource_id IS NULL", false)
+  }
+
   def original_content
     content_for(master_language)
   end
