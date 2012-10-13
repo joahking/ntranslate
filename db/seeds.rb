2f3f9c4cf462dd0015887e14abf38a4a63e0156b
Erick Dennis <erick.dennis@xing.com>
2012-10-13 08:34:28 +0200

    Make seed data idempotent

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
+# Users
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.connection.execute('TRUNCATE TABLE users')
+
 puts 'SETTING UP DEFAULT USER LOGIN'
 user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
 user.confirm!
