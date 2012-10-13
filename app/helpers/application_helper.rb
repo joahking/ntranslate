5410f471bdcdc16c1cb43bdd8e8202241f16a1bb
Jean Carlos Meninno <eth0.lo@gmail.com>
2012-10-13 16:36:23 +0200

    no avatar in the meanwhile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   def welcome_user
-    img = if current_user.avatar
-            image_tag current_user.avatar, :size => "32x32", :alt => current_user.name
-          end
+    #img = if current_user.avatar
+    #        image_tag current_user.avatar, :size => "32x32", :alt => current_user.name
+    #      end
     "Welcome #{img} #{current_user.name}".html_safe
   end
 end
