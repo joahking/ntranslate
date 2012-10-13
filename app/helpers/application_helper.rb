b0f7befa10419a69bcabd1f4937fcb59738bdf94
Jean Carlos Meninno <eth0.lo@gmail.com>
2012-10-13 16:43:14 +0200

    missing variable fixwed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
   def welcome_user
+  	img = nil
     #img = if current_user.avatar
     #        image_tag current_user.avatar, :size => "32x32", :alt => current_user.name
     #      end
