d6c1c8bb8b9e7e5d9c13381103fc8c4345880425
Joaquin Rivera Padron <joahking@gmail.com>
2012-10-14 21:49:48 +0200

    projects show and edit nav bar fixed

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-  set_tab :home, :except => %w(show, edit)
+  set_tab :home, :except => %w(show edit)
 
   before_filter :authorize, :except => [:index, :show]
 
